2e8c527ad3518eebb52e372ac14c8cd5e419cfa4
ilhamfw <ilham.fw18@gmail.com>
2024-03-07 13:09:19 +0700

    add runtime/debug

--- main.go
+++ main.go
@@ -1,76 +1,76 @@
 package main
 
 import (
-	"log"
-	"net/http"
+    "log"
+    "net/http"
+    "runtime/debug"
 
-	socketio "github.com/googollee/go-socket.io"
+    socketio "github.com/googollee/go-socket.io"
 )
 
 type Message struct {
-	Sender  string `json:"sender"`
-	Content string `json:"content"`
+    Sender  string `json:"sender"`
+    Content string `json:"content"`
 }
 
 func main() {
-	server := socketio.NewServer(nil)
-	var connectedUsers = make(map[string]socketio.Conn)
+    server := socketio.NewServer(nil)
+    var connectedUsers = make(map[string]socketio.Conn)
 
-	server.OnConnect("/", func(s socketio.Conn) error {
-		s.SetContext("")
-		connectedUsers[s.ID()] = s
-		log.Println("connected:", s.ID())
-		// Broadcast the new user's ID to all connected users
-		for _, conn := range connectedUsers {
-			if conn.ID() != s.ID() {
-				conn.Emit("user_connected", s.ID())
-			}
-		}
-		return nil
-	})
+    server.OnConnect("/", func(s socketio.Conn) error {
+        s.SetContext("")
+        connectedUsers[s.ID()] = s
+        log.Println("connected:", s.ID())
+        // Broadcast the new user's ID to all connected users
+        for _, conn := range connectedUsers {
+            if conn.ID() != s.ID() {
+                conn.Emit("user_connected", s.ID())
+            }
+        }
+        return nil
+    })
 
-	server.OnDisconnect("/", func(s socketio.Conn, reason string) {
-		log.Println("closed", reason)
-		// Remove the user from the connected users map
-		delete(connectedUsers, s.ID())
-		// Broadcast to all users that someone has disconnected
-		for _, conn := range connectedUsers {
-			conn.Emit("user_disconnected", s.ID())
-		}
-	})
+    // Panic recovery middleware
+    server.OnError("/", func(s socketio.Conn, e error) {
+        log.Printf("recovering from panic, reason: %v", e)
+        debug.PrintStack()
+        s.Emit("error", "An unexpected error occurred")
+    })
 
-	server.OnEvent("/chat", "msg", func(s socketio.Conn, msg Message) {
-		// Broadcast message to all clients except the sender
-		for _, user := range connectedUsers {
-			if user.ID() != s.ID() {
-				user.Emit("msg", msg)
-			}
-		}
-	})
-	
-	
-	
+    server.OnDisconnect("/", func(s socketio.Conn, reason string) {
+        log.Println("closed", reason)
+        // Remove the user from the connected users map
+        delete(connectedUsers, s.ID())
+        // Broadcast to all users that someone has disconnected
+        for _, conn := range connectedUsers {
+            conn.Emit("user_disconnected", s.ID())
+        }
+    })
 
-	server.OnEvent("/chat", "bye", func(s socketio.Conn) {
-		log.Println("bye from:", s.ID())
-		// You can perform any cleanup here if needed
-		s.Close()
-	})
+    server.OnEvent("/chat", "msg", func(s socketio.Conn, msg Message) {
+        // Broadcast message to all clients except the sender
+        for _, user := range connectedUsers {
+            if user.ID() != s.ID() {
+                user.Emit("msg", msg)
+            }
+        }
+    })
 
-	server.OnError("/", func(s socketio.Conn, e error) {
-		log.Println("meet error:", e)
-	})
+    server.OnEvent("/chat", "bye", func(s socketio.Conn) {
+        log.Println("bye from:", s.ID())
+        s.Close()
+    })
 
-	go func() {
-		if err := server.Serve(); err != nil {
-			log.Fatalf("socketio listen error: %s\n", err)
-		}
-	}()
-	defer server.Close()
+    go func() {
+        if err := server.Serve(); err != nil {
+            log.Fatalf("socketio listen error: %s\n", err)
+        }
+    }()
+    defer server.Close()
 
-	http.Handle("/socket.io/", server)
-	http.Handle("/", http.FileServer(http.Dir("asset")))
+    http.Handle("/socket.io/", server)
+    http.Handle("/", http.FileServer(http.Dir("asset")))
 
-	log.Println("Serving at localhost:8000...")
-	log.Fatal(http.ListenAndServe(":8000", nil))
+    log.Println("Serving at localhost:8000...")
+    log.Fatal(http.ListenAndServe(":8000", nil))
 }
