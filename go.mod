785f0612c7c8a89c07332fd43f2a1bd1575a5439
ilhamfw <ilham.fw18@gmail.com>
2024-03-07 13:05:16 +0700

    first commit

---
+++ go.mod
@@ -0,0 +1,10 @@
+module socketio-project
+
+go 1.21.1
+
+require (
+	github.com/gofrs/uuid v4.0.0+incompatible // indirect
+	github.com/gomodule/redigo v1.8.4 // indirect
+	github.com/googollee/go-socket.io v1.7.0 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
+)
---
+++ main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	socketio "github.com/googollee/go-socket.io"
+)
+
+type Message struct {
+	Sender  string `json:"sender"`
+	Content string `json:"content"`
+}
+
+func main() {
+	server := socketio.NewServer(nil)
+	var connectedUsers = make(map[string]socketio.Conn)
+
+	server.OnConnect("/", func(s socketio.Conn) error {
+		s.SetContext("")
+		connectedUsers[s.ID()] = s
+		log.Println("connected:", s.ID())
+		// Broadcast the new user's ID to all connected users
+		for _, conn := range connectedUsers {
+			if conn.ID() != s.ID() {
+				conn.Emit("user_connected", s.ID())
+			}
+		}
+		return nil
+	})
+
+	server.OnDisconnect("/", func(s socketio.Conn, reason string) {
+		log.Println("closed", reason)
+		// Remove the user from the connected users map
+		delete(connectedUsers, s.ID())
+		// Broadcast to all users that someone has disconnected
+		for _, conn := range connectedUsers {
+			conn.Emit("user_disconnected", s.ID())
+		}
+	})
+
+	server.OnEvent("/chat", "msg", func(s socketio.Conn, msg Message) {
+		// Broadcast message to all clients except the sender
+		for _, user := range connectedUsers {
+			if user.ID() != s.ID() {
+				user.Emit("msg", msg)
+			}
+		}
+	})
+	
+	
+	
+
+	server.OnEvent("/chat", "bye", func(s socketio.Conn) {
+		log.Println("bye from:", s.ID())
+		// You can perform any cleanup here if needed
+		s.Close()
+	})
+
+	server.OnError("/", func(s socketio.Conn, e error) {
+		log.Println("meet error:", e)
+	})
+
+	go func() {
+		if err := server.Serve(); err != nil {
+			log.Fatalf("socketio listen error: %s\n", err)
+		}
+	}()
+	defer server.Close()
+
+	http.Handle("/socket.io/", server)
+	http.Handle("/", http.FileServer(http.Dir("asset")))
+
+	log.Println("Serving at localhost:8000...")
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
